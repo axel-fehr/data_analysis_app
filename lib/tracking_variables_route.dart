dc939b2b9d5509450b2b430146be87aa70dc10e8
Axel Fehr <axel.fehr@irubis.com>
2019-10-19 18:53:01 +0800

    add code for basic screen layout when no variables have been created yet

--- lib/tracking_variables_route.dart
+++ lib/tracking_variables_route.dart
@@ -9,11 +9,55 @@ class TrackingVariablesRoute extends StatelessWidget {
         title: Text("Tracked Variables"),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('+'),
-          onPressed: null,
+        child: Text(
+          'You haven\'t created a tracking variable yet.'
+          '\nGo ahead and create one!',
+          style: TextStyle(fontSize: 16),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text(
+          '+',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+        ),
+        onPressed: null,
       ),
     );
   }
 }
+
+/*
+class TrackingVariablesRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tracked Variables"),
+      ),
+      body: Center(
+        widthFactor: 1.0,
+        child: SizedBox(
+          width: 300.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Variable name',
+                  ),
+                ),
+              ),
+              RaisedButton(
+                child: Text('+'),
+                onPressed: null,
+                ),
+              ],
+            )
+        )
+      ),
+    );
+  }
+}*/
