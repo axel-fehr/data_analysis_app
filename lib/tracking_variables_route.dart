59385b14a091b0b17e0b401984515fd53fd165cf
Axel Fehr <axel.fehr@irubis.com>
2019-11-02 22:40:56 +0800

    add unfinished class for the list of tracking variables

--- lib/tracking_variables_route.dart
+++ lib/tracking_variables_route.dart
@@ -66,6 +66,45 @@ class TrackingVariablesRoute extends StatelessWidget {
   }
 }
 
+class TrackingVariablesList extends StatefulWidget {
+  @override
+  TrackingVariablesListState createState() => TrackingVariablesListState();
+}
+
+class TrackingVariablesListState extends State<TrackingVariablesList> {
+  final _suggestions = <Text>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  // #docregion _buildRow
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
 /*
 // #docregion MyApp
 class MyApp extends StatelessWidget {
