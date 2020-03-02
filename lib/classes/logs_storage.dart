56c4542e23b1f62954851832d23b9f877a0ac41c
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-03-02 22:54:53 +0800

    save logs in a txt file to disk

---
+++ lib/classes/logs_storage.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+// Note: Code from this source: https://flutter.dev/docs/cookbook/persistence/reading-writing-files
+
+class LogsStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/logs.txt');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      String contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$counter');
+  }
+}
--- lib/classes/tracker.dart
+++ lib/classes/tracker.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
 import './log.dart';
+import './logs_storage.dart';
 
 class Tracker {
   String _name;
   String _type;
   List<Log> _logs = [];
+  LogsStorage _logStorage; // TODO: initialize object with name of file with the stored logs, name will be based on tracker name
 
   Tracker(String name, String type) {
     _name = name;
@@ -18,5 +24,6 @@ class Tracker {
   void addLog(logValue) {
     print("adding log, value: $logValue");
     _logs.add(Log(logValue));
+    _logStorage.writeCounter(logValue);
   }
 }
