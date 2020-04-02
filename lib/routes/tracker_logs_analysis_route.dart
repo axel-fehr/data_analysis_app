16da26bfa3f3723d6cbc6efe43ad4e765f9125ee
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-04-03 00:09:45 +0800

    add pencil icons symbolizing editing to list of logs that can show an alert dialog on button tap (will be used to delete and change logs, functionality still to be implemented)

--- lib/routes/tracker_logs_analysis_route.dart
+++ lib/routes/tracker_logs_analysis_route.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../providers/tracker_list.dart';
 import '../classes/tracker.dart';
@@ -37,7 +38,7 @@ class LogList extends StatelessWidget {
         children: <Widget>[
           Text('Logs:'),
           Container(
-            child: LogListView(_trackerName),
+            child: LogListWithEditButtonsListView(_trackerName),
             height: 500,
           )
         ],
@@ -47,28 +48,89 @@ class LogList extends StatelessWidget {
   }
 }
 
-class LogListView extends StatelessWidget {
+class LogListWithEditButtonsListView extends StatelessWidget {
   final String _trackerName;
 
-  LogListView(this._trackerName);
+  LogListWithEditButtonsListView(this._trackerName);
 
-  @override
-  Widget build(BuildContext context) {
+  /// Returns the tracker that has the name given by the field '_trackerName'.
+  Tracker getTrackerWithTrackerName(BuildContext context) {
     List<Tracker> listOfTrackers = Provider.of<TrackerList>(context).trackers;
 
     Tracker matchingTracker = listOfTrackers.singleWhere(
         (tracker) => tracker.name == _trackerName,
         orElse: () => throw ('Provided tracker name not found in list.'));
 
+    return matchingTracker;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Tracker correspondingTracker = getTrackerWithTrackerName(context);
     List<Padding> logValueList = [];
 
-    matchingTracker.logs.forEach((log) => logValueList.add(Padding(
+    correspondingTracker.logs.forEach((log) => logValueList.add(Padding(
           padding: const EdgeInsets.only(top: 8.0),
-          child: Text(log.value.toString()),
+          child: LogWithEditButton(log),
         )));
 
     return ListView(
       children: logValueList,
+    );
+  }
+}
+
+class LogWithEditButton extends StatelessWidget {
+  final Log _log;
+
+  LogWithEditButton(this._log);
+  //TODO: let's have edit / pencil icon next to each item on button press, when pressed, popup dialog opens where log can be deleted or value can be changed
+  void showLogEditAlertDialog(BuildContext context) {
+    // TODO: change the alert dialog depending on the type of the tracker (create one separate widget for each alert dialog (for each tracker type))
+    Widget changeLogButton = FlatButton(
+      child: Text('Change to False'),
+      onPressed: () {
+        print('pressed change'); // TODO: implement change of log value (update DB as well!)
+        Navigator.of(context).pop();
+      },
+    );
+    // TODO: consider using a provider for the log and tracker database, needed at a lot of places, use provider here then
+    Widget deleteLogButton = FlatButton(
+      child: Icon(Icons.delete),
+      onPressed: () {
+        print('pressed delete'); // TODO: implement deletion of log (update DB as well!)
+        Navigator.of(context).pop();
+      },
+    );
+
+    // set up the AlertDialog
+    CupertinoAlertDialog alert = CupertinoAlertDialog(
+      title: Text('Edit Log'),
+      actions: [
+        changeLogButton,
+        deleteLogButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(child: Text(_log.value.toString())),
+        InkWell(
+          child: Icon(Icons.create),
+          onTap: () => showLogEditAlertDialog(context), // print('edit'),
+        ),
+      ],
     );
   }
 }
