b2d98b9b4cea70494ba82efe17a6b6648fc9b1c0
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-04-18 18:23:09 +0200

    fixed bug that caused logs of trackers with spaces in their names not to be saved

--- lib/classes/log_database.dart
+++ lib/classes/log_database.dart
@@ -7,7 +7,7 @@ import './log.dart';
 
 class LogDatabase {
   final String _trackerName;
-  final String _databaseName;
+  final String _databaseNameInDoubleQuotationMarks;
   String _databasePath;
   Future<Database> _database;
 
@@ -16,7 +16,10 @@ class LogDatabase {
   /// Arguments:
   /// [_trackerName] : the name of the tracker whose logs are stored in
   ///                  the database.
-  LogDatabase(this._trackerName) : _databaseName = _trackerName + '_logs';
+  // double quotation marks are used to handle names with spaces
+  LogDatabase(this._trackerName)
+      : _databaseNameInDoubleQuotationMarks =
+            '"' + _trackerName + '_logs' + '"';
 
   /// Initializes the member variable [_database].
   ///
@@ -26,12 +29,14 @@ class LogDatabase {
   /// it is asynchronous.
   Future<void> setUpDatabase() async {
     WidgetsFlutterBinding.ensureInitialized();
-    _databasePath = join(await getDatabasesPath(), _trackerName + '_log_database.db');
+    _databasePath =
+        join(await getDatabasesPath(), _trackerName + '_log_database.db');
     _database = openDatabase(
       _databasePath,
       onCreate: (db, version) {
         // TODO: adjust the type used to store values based on the type of tracker
-        String command = 'CREATE TABLE IF NOT EXISTS $_databaseName(timeStamp DATETIME PRIMARY KEY, value INTEGER)';
+        String command =
+            'CREATE TABLE IF NOT EXISTS $_databaseNameInDoubleQuotationMarks(timeStamp DATETIME PRIMARY KEY, value INTEGER)';
         return db.execute(command);
       },
       version: 1,
@@ -48,7 +53,7 @@ class LogDatabase {
     final Database db = await _database;
 
     await db.insert(
-      _databaseName,
+      _databaseNameInDoubleQuotationMarks,
       log.toMap(),
       conflictAlgorithm: ConflictAlgorithm.ignore,
     );
@@ -58,7 +63,7 @@ class LogDatabase {
     final Database db = await _database;
 
     await db.update(
-      _databaseName,
+      _databaseNameInDoubleQuotationMarks,
       log.toMap(),
       where: 'timeStamp = ?',
       whereArgs: [log.timeStamp.toIso8601String()],
@@ -74,7 +79,7 @@ class LogDatabase {
     final Database db = await _database;
 
     await db.delete(
-      _databaseName,
+      _databaseNameInDoubleQuotationMarks,
       where: 'timeStamp = ?',
       whereArgs: [timeStampOfLogToDelete.toIso8601String()],
     );
@@ -84,7 +89,8 @@ class LogDatabase {
   Future<List<Log>> readLogs() async {
     final Database db = await _database;
 
-    final List<Map<String, dynamic>> maps = await db.query(_databaseName);
+    final List<Map<String, dynamic>> maps =
+        await db.query(_databaseNameInDoubleQuotationMarks);
 
     // Convert the List<Map<String, dynamic> into a List<Log>.
     return List.generate(maps.length, (i) {
@@ -98,12 +104,10 @@ class LogDatabase {
   bool mapIntLogValueFromDatabaseToBool(int logValueFromDatabase) {
     if (logValueFromDatabase == 0) {
       return false;
-    }
-    else if (logValueFromDatabase == 1) {
+    } else if (logValueFromDatabase == 1) {
       return true;
-    }
-    else {
-      throw('Integers other than 0 and 1 are not converted to Booleans.');
+    } else {
+      throw ('Integers other than 0 and 1 are not converted to Booleans.');
     }
   }
 }
