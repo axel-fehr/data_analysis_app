3f32202c77ecd64f5fca63bd6ae781671d814485
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-04-19 18:03:55 +0200

    also list tracker names with which correlations could not be computed and state that more data is needed and other minor changes

--- lib/widgets/log_statistics_widgets/for_binary_trackers/binary_tracker_correlations.dart
+++ lib/widgets/log_statistics_widgets/for_binary_trackers/binary_tracker_correlations.dart
@@ -8,11 +8,103 @@ import '../../../classes/tracker.dart';
 import '../../../statistics_utils/correlation.dart';
 
 class BinaryTrackerCorrelations extends StatelessWidget {
-  final String trackerName; // TODO: more descriptive and less ambiguous name
+  final String nameOfTrackerBeingAnalyzed;
 
   BinaryTrackerCorrelations({
-    @required this.trackerName,
+    @required this.nameOfTrackerBeingAnalyzed,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    List<Tracker> listOfTrackers = Provider.of<TrackerList>(context).trackers;
+
+    // TODO: check how this looks and style it accordingly
+    if (listOfTrackers.length == 1) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text('Cannot be computed because no other trackers exist.'),
+      );
+    } else {
+      Tracker trackerBeingAnalyzed = listOfTrackers
+          .singleWhere((tracker) => tracker.name == nameOfTrackerBeingAnalyzed);
+
+      if (trackerBeingAnalyzed.logs.isNotEmpty) {
+        return Expanded(
+          child: ListOfCorrelationsWithOtherTrackers(
+            nameOfTrackerBeingAnalyzed: nameOfTrackerBeingAnalyzed,
+          ),
+        );
+      } else {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('Correlations with other trackers will be displayed '
+              'for you here when you have added enough logs.'),
+        );
+      }
+    }
+  }
+}
+
+class ListOfCorrelationsWithOtherTrackers extends StatelessWidget {
+  final String nameOfTrackerBeingAnalyzed;
+
+  ListOfCorrelationsWithOtherTrackers({this.nameOfTrackerBeingAnalyzed});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Tracker> listOfTrackers = Provider.of<TrackerList>(context).trackers;
+
+    Map sortedTrackerNamesAndCorrelations =
+        getCorrelationsWithOtherTrackersOrderedByMagnitude(context);
+    List<double> sortedCorrelations =
+        sortedTrackerNamesAndCorrelations['correlations'];
+    List<String> sortedTrackerNames =
+        sortedTrackerNamesAndCorrelations['trackerNames'];
+
+    if (sortedCorrelations.length != sortedTrackerNames.length) {
+      throw ('Number of correlations and number of returned tracker names is'
+          'not equal.');
+    }
+
+    List<Widget> correlationTableRows = [];
+
+    for (int i = 0; i < sortedCorrelations.length; i++) {
+      if (sortedCorrelations[i].isNaN) {
+        correlationTableRows.add(TrackerCorrelationListTile(
+          sortedTrackerNames[i],
+          moreDataNeeded: true,
+        ));
+      } else {
+        correlationTableRows.add(TrackerCorrelationListTile(
+          sortedTrackerNames[i],
+          correlation: sortedCorrelations[i],
+        ));
+      }
+    }
+
+    bool trackersWithNoCorrelationValuesExist;
+    trackersWithNoCorrelationValuesExist =
+        listOfTrackers.length - 1 != sortedCorrelations.length;
+    if (trackersWithNoCorrelationValuesExist) {
+      for (int i = 0; i < listOfTrackers.length; i++) {
+        // TODO: add comments to explain stuff or rename variables appropriately if possible
+        if (!sortedTrackerNames.contains(listOfTrackers[i].name)) {
+          correlationTableRows.add(TrackerCorrelationListTile(
+            listOfTrackers[i].name,
+            moreDataNeeded: true,
+          ));
+        }
+      }
+    }
+
+    return Container(
+      child: ListView(
+        children: correlationTableRows,
+      ),
+    );
+    // TODO: would be nice to display how many values the correlation was computed with
+    // TODO: is it possible to give some kind of 'confidence' about the correlation based on the number of samples? If yes, do that
+  }
 
   /// Returns a map containing the correlation coefficients between the logs
   /// of the tracker with the given tracker name and the logs of the other
@@ -30,8 +122,8 @@ class BinaryTrackerCorrelations extends StatelessWidget {
     listOfTrackers.trackers
         .forEach((tracker) => checkIfMultipleLogsOnSameDay(tracker));
     List<Tracker> trackerListCopy = List<Tracker>.from(listOfTrackers.trackers);
-    Tracker trackerCorrespondingToLogs =
-        trackerListCopy.singleWhere((tracker) => tracker.name == trackerName);
+    Tracker trackerCorrespondingToLogs = trackerListCopy
+        .singleWhere((tracker) => tracker.name == nameOfTrackerBeingAnalyzed);
 
     // avoids computing the correlation with the tracker itself
     trackerListCopy.remove(trackerCorrespondingToLogs);
@@ -91,47 +183,34 @@ class BinaryTrackerCorrelations extends StatelessWidget {
       throw ('Found a tracker whose logs are not all from different dates.');
     }
   }
+}
+
+class TrackerCorrelationListTile extends StatelessWidget {
+  final String trackerName;
+  final double correlation;
+  final bool moreDataNeeded;
+
+  TrackerCorrelationListTile(this.trackerName,
+      {this.correlation = double.nan, this.moreDataNeeded = false});
 
   @override
   Widget build(BuildContext context) {
-    List<Tracker> listOfTrackers = Provider.of<TrackerList>(context).trackers;
-    Tracker trackerCorrespondingToLogs =
-        listOfTrackers.singleWhere((tracker) => tracker.name == trackerName);
+    Text displayedTrackerName = Text(trackerName + ':');
 
-    if (trackerCorrespondingToLogs.logs.isNotEmpty) {
-      Map sortedTrackerNamesAndCorrelations =
-          getCorrelationsWithOtherTrackersOrderedByMagnitude(context);
-      List<double> sortedCorrelations =
-          sortedTrackerNamesAndCorrelations['correlations'];
-      List<String> trackerNames =
-          sortedTrackerNamesAndCorrelations['trackerNames'];
-
-      if (sortedCorrelations.length != trackerNames.length) {
-        throw ('Number of correlations and number of returned tracker names is'
-            'not equal.');
-      }
-
-      // TODO: make this all look pretty!
-      if (sortedCorrelations.isEmpty) {
-        return Text(
-            'No correlations to display.'); // TODO: add an explanation here and what is needed to display correlations
-      } else {
-        // TODO: use list of list tiles here where each list tile contains the tracker name and the correlation
-        return Container(
-          child: ListView(
-            children: List<Widget>.generate(sortedCorrelations.length,
-                (index) => Text(sortedCorrelations[index].toString())),
-          ),
-          height: 100,
-          width: double.infinity,
-        );
-        // TODO: would be nice to display how many values the correlation was computed with
-        // TODO: is it possible to give some kind of 'confidence' about the correlation based on the number of samples? If yes, do that
-      }
+    Text displayedCorrelationValue;
+    if (moreDataNeeded || correlation.isNaN) {
+      displayedCorrelationValue = Text('More data needed');
     } else {
-      return Text('Correlations cannot be computed because this tracker does '
-          'not have any logs. Add logs and the app will display '
-          'correlations for you here');
+      displayedCorrelationValue = Text(correlation.toString());
     }
+
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          displayedTrackerName,
+          displayedCorrelationValue,
+        ],
+      ),
+    );
   }
 }
