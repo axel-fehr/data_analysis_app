2713d1e97fe6fb2b8a6e2ef1ba9e5141c70a5799
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-04-21 13:03:58 +0200

    tracker cannot be added if the entered name already exists and a warning is shown when that's the case. Also, trackers are not added anymore when the entered tracker name at the button tap is null or an empty string

--- lib/routes/tracker_list_route.dart
+++ lib/routes/tracker_list_route.dart
@@ -45,30 +45,6 @@ class Content extends StatelessWidget {
 }
 
 class AddTrackerButton extends StatelessWidget {
-  Future<String> createAlertDialogToAddTracker(BuildContext context) {
-    TextEditingController customController = TextEditingController();
-
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Tracker name'),
-            content: TextField(
-              controller: customController,
-            ),
-            actions: <Widget>[
-              MaterialButton(
-                elevation: 5.0,
-                child: Text('Add'),
-                onPressed: () {
-                  Navigator.of(context).pop(customController.text.toString());
-                },
-              )
-            ],
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
@@ -77,11 +53,91 @@ class AddTrackerButton extends StatelessWidget {
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
       ),
       onPressed: () {
-        createAlertDialogToAddTracker(context).then((onValue) {
-          final trackerListObject = Provider.of<TrackerList>(context);
-          trackerListObject.addTracker(onValue);
+        showAlertDialogToAddTracker(context).then((onValue) {
+          if (onValue != null && onValue != '') {
+            final trackerListObject = Provider.of<TrackerList>(context);
+            trackerListObject.addTracker(onValue);
+          }
         });
       },
+    );
+  }
+
+  Future<String> showAlertDialogToAddTracker(BuildContext context) {
+    TextEditingController customController = TextEditingController();
+
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AddTrackerAlertDialog();
+        });
+  }
+}
+
+class AddTrackerAlertDialog extends StatefulWidget {
+  @override
+  _AddTrackerAlertDialogState createState() => _AddTrackerAlertDialogState();
+}
+
+class _AddTrackerAlertDialogState extends State<AddTrackerAlertDialog> {
+  TextEditingController customController = TextEditingController();
+
+  // determines whether a warning is shown that tells the user that he must
+  // not add a tracker with a name that already exists
+  bool showTrackerNameWarning = false;
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> trackerNames = Provider.of<TrackerList>(context).trackerNames;
+
+    return AlertDialog(
+      title: Text('Tracker name'),
+      content: Container(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: customController,
+              onChanged: (String enteredText) {
+                if (trackerNames.contains(customController.text.toString())) {
+                  setState(() {
+                    showTrackerNameWarning = true;
+                  });
+                }
+                // removes the warning if entered text is not an existing name
+                else if (showTrackerNameWarning = true) {
+                  setState(() {
+                    showTrackerNameWarning = false;
+                  });
+                }
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Visibility(
+                child: Text(
+                  'Name already exists!',
+                  style: TextStyle(color: Colors.redAccent),
+                ),
+                visible: showTrackerNameWarning,
+              ),
+            ),
+          ],
+        ),
+        height: 90,
+      ),
+      actions: <Widget>[
+        MaterialButton(
+          elevation: 5.0,
+          child: Text('Add'),
+          onPressed: () {
+            String enteredTrackerName = customController.text.toString();
+            // if the tracker name does not already exist, add the tracker
+            if (!trackerNames.contains(enteredTrackerName)) {
+              Navigator.of(context).pop(enteredTrackerName);
+            }
+          },
+        )
+      ],
     );
   }
 }
