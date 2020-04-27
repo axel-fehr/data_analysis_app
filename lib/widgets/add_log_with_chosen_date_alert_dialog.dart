b209162657c374cf521af2248d95fe09eab35c47
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-04-27 13:21:18 +0200

    fix bug causing the add log buttons not to get disabled when there are logs with future dates by making it impossible to add logs with future dates

--- lib/widgets/add_log_with_chosen_date_alert_dialog.dart
+++ lib/widgets/add_log_with_chosen_date_alert_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tracking_app/utils/general.dart';
 
 import 'select_date_calendar_view.dart';
 import '../classes/tracker.dart';
@@ -19,6 +20,7 @@ class _AddLogWithChosenDateAlertDialogState
   SelectLogValueSection selectLogValueSection;
   SelectDateCalendarView calendarView = SelectDateCalendarView();
   Color _addLogButtonFontColor = Colors.black45;
+  bool _userSelectedFutureDate = false;
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +31,48 @@ class _AddLogWithChosenDateAlertDialogState
       // updates the font color of the 'Add' button to indicate the log can be
       // added, now that the date has been selected
       setState(() {
-        _addLogButtonFontColor = Colors.blue;
+        DateTime currentDate = convertTimeStampToDate(DateTime.now());
+        _userSelectedFutureDate =
+            calendarView.selectedDate.isAfter(currentDate) ? true : false;
+
+        // changes the color of the button depending on whether the chosen
+        // date is valid or not and whether the log can be added as a signal to
+        // the user
+        _addLogButtonFontColor =
+            _userSelectedFutureDate ? Colors.black45 : Colors.blue;
       });
     };
+
+    // warning that will be shown when user selects future date because
+    // logs with future dates are not accepted
+    Widget doNotChooseFutureDateWarning = Visibility(
+        visible: _userSelectedFutureDate,
+        child: Flexible(
+          child: Column(
+            children: <Widget>[
+              const Text(
+                'Please do not choose a future date.',
+                style: TextStyle(color: Colors.redAccent),
+              ),
+            ],
+          ),
+        ));
+
+    FlatButton addLogWithChosenDateButton = FlatButton(
+      child: Text(
+        'Add',
+        style: TextStyle(color: _addLogButtonFontColor),
+      ),
+      onPressed: () {
+        if (calendarView.userSelectedDate && !_userSelectedFutureDate) {
+          Log createdLog = Log(
+              Log.yesOrNoToBool(selectLogValueSection.selectedValue),
+              timeStamp: calendarView.selectedDate);
+          print('chosen date: ${createdLog.toString()}');
+          Navigator.of(context).pop(createdLog);
+        }
+      },
+    );
 
     return AlertDialog(
         title: const Text('Choose date and value'),
@@ -45,24 +86,12 @@ class _AddLogWithChosenDateAlertDialogState
               child: calendarView,
             ),
             selectLogValueSection,
-            Align(
-              child: FlatButton(
-                child: Text(
-                  'Add',
-                  style: TextStyle(color: _addLogButtonFontColor),
-                ),
-                onPressed: () {
-                  if (calendarView.userSelectedDate) {
-                    Log createdLog = Log(
-                        Log.yesOrNoToBool(
-                            selectLogValueSection.selectedValue),
-                        timeStamp: calendarView.selectedDate);
-                    print('chosen date: ${createdLog.toString()}');
-                    Navigator.of(context).pop(createdLog);
-                  }
-                },
-              ),
-              alignment: Alignment.centerRight,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                doNotChooseFutureDateWarning,
+                addLogWithChosenDateButton,
+              ],
             )
           ],
         ));
@@ -110,8 +139,10 @@ class _SelectLogValueSectionState extends State<SelectLogValueSection> {
                     widget._selectedValue = newValue;
                   });
                 },
-                items: <String>[Log.boolToYesOrNo(true), Log.boolToYesOrNo(false)]
-                    .map<DropdownMenuItem<String>>((String value) {
+                items: <String>[
+                  Log.boolToYesOrNo(true),
+                  Log.boolToYesOrNo(false)
+                ].map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
                     child: Text(value),
