376545f6b103175c2f905dd02fc81fb2e41a6461
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-05-09 16:42:55 +0200

    add disclaimers or warnings that state that the provided info is not medical advice and that correlation does not imply causation

--- lib/widgets/log_statistics_widgets/for_binary_trackers/binary_tracker_log_stats.dart
+++ lib/widgets/log_statistics_widgets/for_binary_trackers/binary_tracker_log_stats.dart
@@ -18,6 +18,12 @@ class LogStatsOfBinaryTracker extends StatelessWidget {
     return Container(
       child: Column(
         children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: DisclaimerOrWarning(
+              text: 'The information provided on this app is not health advice.',
+            ),
+          ),
           const SectionHeadline(
             textToDisplay: 'Overall Statistics',
           ),
@@ -26,6 +32,12 @@ class LogStatsOfBinaryTracker extends StatelessWidget {
             textToDisplay: 'Correlations',
           ),
           BinaryTrackerCorrelations(nameOfTrackerBeingAnalyzed: trackerName),
+          const Padding(
+            padding: EdgeInsets.only(top: 4.0),
+            child: DisclaimerOrWarning(
+              text: 'Correlation does not imply causation.',
+            ),
+          ),
 //          BinaryTrackerStatsOverChosenPeriod(
 //            trackerName: trackerName,
 //            sectionHeadlineTextStyle: sectionHeadlineTextStyle,
@@ -33,6 +45,37 @@ class LogStatsOfBinaryTracker extends StatelessWidget {
         ],
       ),
       height: 300,
+    );
+  }
+}
+
+class DisclaimerOrWarning extends StatelessWidget {
+  final String text;
+
+  const DisclaimerOrWarning({@required this.text});
+
+  static const Color _color = Colors.black45;
+  static const double _size = 12;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.0),
+      child: Wrap(
+        direction: Axis.horizontal,
+        alignment: WrapAlignment.center,
+        children: <Widget>[
+          const Icon(
+            Icons.warning,
+            size: _size,
+            color: _color,
+          ),
+          Text(
+            text,
+            style: const TextStyle(fontSize: _size, color: _color),
+          ),
+        ],
+      ),
     );
   }
 }
