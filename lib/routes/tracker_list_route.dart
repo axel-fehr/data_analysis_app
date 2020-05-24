a8e6e302bffb80a08027103b387c66c16d3ffdbc
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-05-24 16:23:50 +0200

    rebuild tracker list when app becomes active again, if the last rebuild happened on a different date to make it possible to use the log buttons again when the app has been in the background since a previous date

--- lib/routes/tracker_list_route.dart
+++ lib/routes/tracker_list_route.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
+import '../utils/general.dart' as utils;
 import '../providers/tracker_list.dart';
 import '../widgets/tracker_list_with_add_log_button_list_view.dart';
 import '../widgets/disclaimer_or_warning.dart';
@@ -12,15 +13,53 @@ class TrackerListRoute extends StatefulWidget {
   _TrackerListRouteState createState() => _TrackerListRouteState();
 }
 
-class _TrackerListRouteState extends State<TrackerListRoute> {
+class _TrackerListRouteState extends State<TrackerListRoute>
+    with WidgetsBindingObserver {
+  // used to store the date of the last widget rebuild.
+  DateTime _dateOfLastRebuild;
+
   @override
   Widget build(BuildContext context) {
+    _dateOfLastRebuild = utils.convertTimeStampToDate(DateTime.now());
     return Scaffold(
       appBar: AppBar(
         title: const Text('Trackers'),
       ),
       body: Content(),
     );
+  }
+
+  /// Rebuilds the widget when the app has been in the background and is resumed
+  /// again, if the date of the last rebuild is not the current date.
+  ///
+  /// This is necessary to make sure the add log buttons become active again on
+  /// the next day (if there isn't a log with that date already), if the app has
+  /// been in the background since a previous date. If this is not done, the add
+  /// log buttons are inactive the next day even when there aren't any logs with
+  /// the current date.
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    DateTime currentDate = utils.convertTimeStampToDate(DateTime.now());
+    if (state == AppLifecycleState.resumed &&
+        _dateOfLastRebuild != currentDate) {
+      // trigger rebuild to update add log buttons
+      setState(() {});
+    }
+  }
+
+  // overwritten to add the observer of the app lifecycle state when the state
+  // is initialized (to trigger the rebuild when the app is active again)
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  // overwritten to also dispose the observer of the app lifecycle state
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
   }
 }
 
