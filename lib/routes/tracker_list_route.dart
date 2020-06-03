7895e6cea2cf8c0652005b7faa3fc157a27b3976
axel-fehr <47457597+axel-fehr@users.noreply.github.com>
2020-06-03 12:41:10 +0200

    move the add tracker alert dialog into a separate file

--- lib/routes/tracker_list_route.dart
+++ lib/routes/tracker_list_route.dart
@@ -2,13 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-import 'package:tracking_app/enumerations/user_interaction.dart';
 
 import '../utils/general.dart' as utils;
 import '../providers/tracker_list.dart';
 import '../widgets/tracker_list_with_add_log_button_list_view.dart';
 import '../widgets/disclaimer_or_warning.dart';
-import '../classes/user_interaction_database.dart';
+import '../widgets/add_tracker_alert_dialog.dart';
 
 class TrackerListRoute extends StatefulWidget {
   @override
@@ -152,91 +151,5 @@ class AddTrackerButton extends StatelessWidget {
         builder: (context) {
           return AddTrackerAlertDialog();
         });
-  }
-}
-
-class AddTrackerAlertDialog extends StatefulWidget {
-  @override
-  _AddTrackerAlertDialogState createState() => _AddTrackerAlertDialogState();
-}
-
-class _AddTrackerAlertDialogState extends State<AddTrackerAlertDialog> {
-  // shown when a tracker with the entered name already exists
-  static const Text _trackerNameWarning = Text(
-    'Name already exists!',
-    style: TextStyle(color: Colors.redAccent),
-  );
-
-  // A hint that tells the user what kind of tracker to enter, to make it
-  // clear that it has to be something answerable with yes or no.
-  static const Text _inputHint = Text(
-    "Must be answerable with 'yes' or 'no'",
-    style: TextStyle(color: Colors.black45),
-  );
-
-  final TextEditingController customController = TextEditingController();
-
-  // determines whether a warning is shown that tells the user that he must
-  // not add a tracker with a name that already exists
-  bool showTrackerNameWarning = false;
-
-  @override
-  Widget build(BuildContext context) {
-    List<String> trackerNames = Provider.of<TrackerList>(context).trackerNames;
-
-    return AlertDialog(
-      title: const Text('What do you want to track?'),
-      content: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            TextField(
-              controller: customController,
-              maxLength: 50,
-              onChanged: (String enteredText) {
-                if (trackerNames.contains(customController.text.toString())) {
-                  setState(() {
-                    showTrackerNameWarning = true;
-                  });
-                }
-                // removes the warning if entered text is not an existing name
-                else if (showTrackerNameWarning = true) {
-                  setState(() {
-                    showTrackerNameWarning = false;
-                  });
-                }
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: showTrackerNameWarning ? _trackerNameWarning : _inputHint,
-            ),
-          ],
-        ),
-      ),
-      actions: <Widget>[
-        MaterialButton(
-          elevation: 5.0,
-          child: const Text(
-            'Add',
-            style: TextStyle(color: Colors.blueAccent),
-          ),
-          onPressed: () {
-            String enteredTrackerName = customController.text.toString();
-            // if the tracker name does not already exist, add the tracker
-            if (!trackerNames.contains(enteredTrackerName)) {
-              UserInteractionDatabase userInteractionDatabase =
-                  Provider.of<UserInteractionDatabase>(context);
-              if (!userInteractionDatabase
-                  .isRecorded(UserInteraction.createdTracker)) {
-                userInteractionDatabase
-                    .recordUserInteraction(UserInteraction.createdTracker);
-              }
-              Navigator.of(context).pop(enteredTrackerName);
-            }
-          },
-        ),
-      ],
-    );
   }
 }
